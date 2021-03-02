323f2aad4010e2080ed7840141f78535e0735751
Ted <tedtaemin@gmail.com>
2021-03-02 11:46:35 -0800

    changed authorization route to authorization folder

---
+++ src/server/controllers/Authorization/AuthorizationController.js
@@ -0,0 +1,16 @@
+
+const authorizationController = {};
+
+authorizationController.checkSignIn = function (req, res, next) {
+  console.log("these are the cookies", req.cookies);
+  if (req.cookies.role === "admin") {
+    console.log('it works')
+    next();     //If session exists, proceed to page
+  } else {
+    var err = new Error("Not logged in!");
+    console.log("not logged in with cookie");
+    next(err);  //Error, trying to access unauthorized page!
+  }
+}
+
+module.exports = authorizationController;
